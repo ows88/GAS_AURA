47cb873b9a12f05b5c14c80f84acdb3774a48f95
Owson <OwsonOMG@users.noreply.github.com>
2024-10-26 17:09:57 +0800

    Aura Player Controller and Subsystem setup

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,31 @@
+// OWS ALL RIGHTS RESERVED
+
+#include "EnhancedInputSubsystems.h"
+#include "Player/AuraPlayerController.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+	
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+	
+	
+	
+}
